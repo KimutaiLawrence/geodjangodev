ed9baa1f892c18afe88de67ee6f4b175c5d08aa1
KimutaiLawrence <kimutai.lawrence19@students.dkut.ac.ke>
2024-05-25 20:18:48 +0800

    added popups

--- reporter/urls.py
+++ reporter/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from reporter.views import HomePageView, county_datasets
+from reporter.views import HomePageView, county_datasets, incidence_datasets  # Import the new view
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('county_data/', county_datasets, name='county'),  # Note the trailing slash here
-    path('incidence_data', HomePageView.as_view(), name='home'),
+    path('incidence_data/', incidence_datasets, name='incidences'),  # Add this line
 ]
--- reporter/views.py
+++ reporter/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.http import HttpResponse
-from .models import Counties
+from .models import Counties, Incidences  # Import Incidences model
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -11,3 +11,7 @@ class HomePageView(TemplateView):
 def county_datasets(request):
     counties = serialize('geojson', Counties.objects.all())
     return HttpResponse(counties, content_type='application/json')
+
+def incidence_datasets(request):
+    incidences = serialize('geojson', Incidences.objects.all())  # Assuming you have an Incidences model
+    return HttpResponse(incidences, content_type='application/json')
